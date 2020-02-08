4cc0cc5bce3d310a55dcdad247ba3ce4a17a600f
lyuning <lyuning@umich.edu>
2020-02-08 16:44:23 -0500

    added initial project files

---
+++ call-manager/call-manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  call-manager
+//
+//  Created by Yuning Liu on 2/8/20.
+//  Copyright © 2020 The Regents of the University of Michigan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ call-manager/call-managerTests/call_managerTests.swift
@@ -0,0 +1,34 @@
+//
+//  call_managerTests.swift
+//  call-managerTests
+//
+//  Created by Yuning Liu on 2/8/20.
+//  Copyright © 2020 The Regents of the University of Michigan. All rights reserved.
+//
+
+import XCTest
+@testable import call_manager
+
+class call_managerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
