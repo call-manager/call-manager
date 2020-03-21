999a2789fbd02e3bb42c8b0cc156f66fd3bd4e9e
duochen13 <duochen@umich.edu>
2020-03-21 00:37:36 -0400

    add search bar func, sos: omg why room text label invalid?

--- call-manager-ios/AudioSinkExample/ContactListVC.swift
+++ call-manager-ios/AudioSinkExample/ContactListVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ContactListVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ContactListVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
     
     @IBOutlet weak var searchBar: UISearchBar!
@@ -18,55 +18,88 @@ class ContactListVC: UIViewController, UITableViewDataSource, UITableViewDelegat
     var contact_temp_email: String = ""
     // a list of personal data
     var people_profile_lists = [PeopleProfile]()
+    var current_people_profile_lists = [PeopleProfile]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUpPeopleProfiles()
+        setUpSearchBar()
     }
     
     private func setUpPeopleProfiles() {
         people_profile_lists.append(PeopleProfile(name: "Doggo", email: "doggo@umich.edu", image: "contact_1"))
-        people_profile_lists.append(PeopleProfile(name: "Doggo's uncle", email: "whyitsacat@gmail.com", image: "contact_2"))
-        people_profile_lists.append(PeopleProfile(name: "A 17lb cat", email: "notspicy@gmail.com", image: "contact_3"))
+        people_profile_lists.append(PeopleProfile(name: "doggo's uncle", email: "whyitsacat@gmail.com", image: "contact_2"))
+        people_profile_lists.append(PeopleProfile(name: "A 17lb cat go", email: "notspicy@gmail.com", image: "contact_3"))
         people_profile_lists.append(PeopleProfile(name: "Doggo's pet", email: "2ha@gmail.com", image: "contact_4"))
+        
+        current_people_profile_lists = people_profile_lists
     }
     
+    private func setUpSearchBar() {
+        searchBar.delegate = self
+    }
+    
+    // Table (Contact List)
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return people_profile_lists.count
+        // return people_profile_lists.count
+        return current_people_profile_lists.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ContactListCell") as? ContactListTableViewCell else {
             return UITableViewCell()
         }
-        cell.contact_name.text = people_profile_lists[indexPath.row].name
-        cell.contact_email.text = people_profile_lists[indexPath.row].email
-        cell.contact_img.image = UIImage(named: people_profile_lists[indexPath.row].image)
+        cell.contact_name.text = current_people_profile_lists[indexPath.row].name
+        cell.contact_email.text = current_people_profile_lists[indexPath.row].email
+        cell.contact_img.image = UIImage(named: current_people_profile_lists[indexPath.row].image)
         return cell
     }
     
     // execute on click
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        // let destinationVC = ContactProfileController()
-        
-        print("!!!@@@@", people_profile_lists[indexPath.row].name)
+        // tableView.deselectRow(at: indexPath, animated: true)
         
         contact_temp_name = people_profile_lists[indexPath.row].name
         contact_temp_email = people_profile_lists[indexPath.row].email
         
-        performSegue(withIdentifier: "showContactProfile", sender: people_profile_lists[indexPath.row])
+        // performSegue(withIdentifier: "showContactProfile", sender: people_profile_lists[indexPath.row])
+        performSegue(withIdentifier: "showContactProfile", sender: self)
     }
         
     // convey data between different vcs through segue, reference: https://learnappmaking.com/pass-data-between-view-controllers-swift-how-to/#forward-segues
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showContactProfile" {
-            let destinate_vc = segue.destination as? ViewController
-            destinate_vc?.temp_contact_name = contact_temp_name
+            // let destinate_vc = segue.destination as? ContactProfileViewController // ViewController
+            // destinate_vc?.contact_temp_name = contact_temp_name
+            // destinate_vc.contact_profile = people_profile_lists[]
+            if let destinate_vc = segue.destination as? ContactProfileViewController {
+                destinate_vc.contact_profile = people_profile_lists[(contactListTable.indexPathForSelectedRow?.row)!]
+            }
         }
     }
+    
+    
+    // Search bar
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // if search bar is empty
+        guard !searchText.isEmpty else { current_people_profile_lists = people_profile_lists; return}
+        
+        // search condition
+        current_people_profile_lists = people_profile_lists.filter({ people_profile -> Bool in
+            guard let text = searchBar.text else { return false }
+            // ignored lower/uppercase difference
+            return people_profile.name.lowercased().contains(text.lowercased())
+        })
+        
+        // reload contact list
+        contactListTable.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        // search based on category, to be added
+    }
+    
 }
 
 class PeopleProfile {
---
+++ call-manager-ios/AudioSinkExample/ContactProfileViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ContactProfileViewController.swift
+//  AudioSinkExample
+//
+//  Created by Duo Chen on 3/20/20.
+//  Copyright © 2020 Twilio Inc. All rights reserved.
+//
+
+import UIKit
+
+class ContactProfileViewController: UIViewController {
+    
+    // profile data from last viewcontroller(ContactListVC)
+    var contact_profile: PeopleProfile?
+    
+    @IBOutlet weak var contact_temp_name_label: UILabel!
+    @IBOutlet weak var contact_temp_email_label: UILabel!
+    @IBOutlet weak var contact_temp_img_view: UIImageView!
+    
+    @IBOutlet weak var callButton: UIButton!
+    
+    @IBAction func callButtonAction(_ sender: Any) {
+        performSegue(withIdentifier: "callSomeone", sender: self)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // render data from ContactListVC through segue, segue id: showContactProfile
+        contact_temp_name_label.text = " \(contact_profile!.name)!"
+        contact_temp_email_label.text = " \(contact_profile!.email)!"
+        contact_temp_img_view.image = UIImage(named: contact_profile!.image)
+        // set profile image to be round
+        contact_temp_img_view.layer.masksToBounds = true
+        contact_temp_img_view.layer.cornerRadius = contact_temp_img_view.bounds.width / 2
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
