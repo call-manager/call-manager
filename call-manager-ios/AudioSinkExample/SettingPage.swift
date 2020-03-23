ed59087ed47d419418630f599ba98aa5e7aa5099
lyuning <lyuning@umich.edu>
2020-03-22 21:56:31 -0400

    minor fixes of UI

--- call-manager-ios/AudioSinkExample/SettingPage.swift
+++ call-manager-ios/AudioSinkExample/SettingPage.swift
@@ -6,3 +6,23 @@
 //  Copyright © 2020 Twilio Inc. All rights reserved.
 //
 
+import UIKit
+
+class SettingPage: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+	
+	
+	@IBAction func changeName(_ sender: UIButton) {
+		// code to change user name
+	}
+	
+	
+	
+	
+}
