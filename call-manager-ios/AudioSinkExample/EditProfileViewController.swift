2389bbac4e9d842a33e8d4e362db500d34bb035d
zihuiq <zihuiq@umich.edu>
2020-04-06 13:43:55 -0400

    bio update

---
+++ call-manager-ios/AudioSinkExample/EditProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditProfileViewController.swift
+//  AudioSinkExample
+//
+//  Created by Zihui Qi on 4/6/20.
+//  Copyright © 2020 Twilio Inc. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
